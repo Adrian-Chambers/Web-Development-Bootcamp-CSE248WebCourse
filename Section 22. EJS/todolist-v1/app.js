612e2872e4cb314e8daf9de38a1225ee70d86892
Adrian C <achambers1541@gmail.com>
2020-08-02 01:51:34 -0400

    more sections

--- Section 22. EJS/todolist-v1/app.js
+++ Section 22. EJS/todolist-v1/app.js
@@ -1,35 +1,41 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-var items = ["Buy Food", "Cook Food", "Eat Food"];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res){
-    var today = new Date();
-    
-    var options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-
-    var day = today.toLocaleDateString("en-US", options);
-
-    res.render("list", {kindOfDay: day, newListItems: items});
+    const day = date.getDate();
+    res.render("list", {listTitle: day, newListItems: items});
 });
 
 app.post("/", function(req, res){
-    var item = req.body.newItem;
+    const item = req.body.newItem;
 
-    items.push(item);
-    
-    res.redirect("/");
+    if(req.body.list === "work"){
+        workItems.push(item);
+        res.redirect("/work");
+    } else{
+        items.push(item);
+        res.redirect("/");
+    }
 });
+
+app.get("/work", function(req, res){
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+})
+
+app.get("/about", function(req, res){
+    res.render("about");
+})
 
 app.listen(3000, function(){
     console.log("Server started on port 3000");
